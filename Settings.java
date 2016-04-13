ee257a805f3351932a7f75d39fd1a4df6f4acc42
Crazy-Dog <ilya-drachev@mail.ru>
2016-04-13 08:10:16 +0300

    Variables changed(again(again))
    Change-Id: I5161cdd4ad6fd6d2d26a38c82a7f7ed0e2f7b58b

---
+++ Settings.java
@@ -0,0 +1,29 @@
+package tanks;
+
+/**
+ * Created by yurashka on 14.03.2016.
+ */
+public class Settings {
+    public static final double SCENE_WIDTH = 600;
+    public static final double SCENE_HEIGHT = 600;
+    public static double playerShipSpeed = 4.0;
+    public static double playerShipHealth = 1000.0;
+    public static double playerMissileSpeed = 15.0;
+    public static double playerMissileHealth = 50.0;
+    public static int player1Lives=3;
+    public static boolean player1IsAlive=false;
+    public static int player2Lives=3;
+    public static boolean player2IsAlive=false;
+    public static boolean gamePaused = false;
+    public static int player1Score=0;
+    public static int player2Score=0;
+    public static boolean automove=false;
+    public static final int ENEMY_SPAWN_RANDOMNESS = 100;
+    public static int enemy = 24;
+    public static int enemyCount =0;
+    public static int enemyTotal=0;
+    public static int BulletsAct=1;
+    public static boolean FIRE = true;
+    public static int fontsSize = 24;
+    }
+
